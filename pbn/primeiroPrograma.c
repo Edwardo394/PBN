bd329a883408e72bf229db5a91a2a2399abda82b
Edwardo394 <69097770+Edwardo394@users.noreply.github.com>
2022-08-04 10:43:11 -0300

    Add files via upload

---
+++ pbn/primeiroPrograma.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    char caractere = 'C';
+    printf("Meu primeiro programa em %c\n", caractere);
+    //Usar %c para caractere e referenciar qual o caractere após a virgula economiza recurso e é mais eficiente que usar o operador +
+}
---
+++ pbn/teste.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int valor1, valor2;
+    printf("Digite dois valores: ");
+    scanf("%d %d", &valor1, &valor2);
+    printf("Você digitou %d e %d\n", valor1, valor2);
+    return 0;
+}
